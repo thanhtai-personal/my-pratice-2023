422eca401e10e433e2898f58fd529f315269433e
kakashi <vanminh020321@gmail.com>
2023-02-27 13:36:37 +0700

    app menu

--- new-pratice-2023/src/components/AppLayout/AppMenu/index.tsx
+++ new-pratice-2023/src/components/AppLayout/AppMenu/index.tsx
@@ -1,26 +1,9 @@
 import React from "react";
-import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
 import useDepsContainer from "hooks/useDepsContainer";
 import { observer } from "mobx-react";
 import { openAnchor } from "actions/layout.actions";
-
-const useStyles = makeStyles({
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: "auto",
-  },
-});
+import AppMenu from "./AppMenu";
 
 export const AnchorType = {
   top: "top",
@@ -34,7 +17,6 @@ export type Anchor = "top" | "left" | "bottom" | "right";
 interface AppDrawerProps {}
 
 const AppDrawer = (props: AppDrawerProps) => {
-  const classes = useStyles();
   const { appMenu } = useDepsContainer();
 
   const toggleDrawer =
@@ -50,39 +32,6 @@ const AppDrawer = (props: AppDrawerProps) => {
       openAnchor(anchor, open);
     };
 
-  const list = (anchor: Anchor) => (
-    <div
-      className={clsx(classes.list, {
-        [classes.fullList]: anchor === "top" || anchor === "bottom",
-      })}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
   return (
     <div>
       {(["left", "right", "top", "bottom"] as Anchor[]).map((anchor) => (
@@ -92,7 +41,7 @@ const AppDrawer = (props: AppDrawerProps) => {
             open={anchor === appMenu.activeAnchor}
             onClose={toggleDrawer(anchor, false)}
           >
-            {list(anchor)}
+            {<AppMenu anchor={anchor} toggleDrawer={toggleDrawer} />}
           </Drawer>
         </React.Fragment>
       ))}
